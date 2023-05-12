5ed482701ba09360caef49cb96cb374aa524acbf
Vinicargui <vinicius.cardoso8888@outlook.com>
2023-05-11 22:04:03 -0300

    Criando aquisições na Api

--- tela-principal/src/componentes/publicacao/index.js
+++ tela-principal/src/componentes/publicacao/index.js
@@ -1,10 +1,39 @@
 
+import "./estilo.css"
+import { Link } from "react-router-dom"
+import{BsPencil, BsFillTrashFill} from 'react-icons/bs'
+
+
+   
+
 function PublicacaoPage({id,titulo,categoria,valor,descricao}){
+
+    
+
+    // button.addEventListener('click',()=>{
+    //    var card = document.querySelector('.produto-card')
+    // })
     return(
-         <div className="form-publicacao">
-            <h1>{titulo}</h1>
-            <h1>{categoria}</h1>
-            <h1>{valor}</h1>
+         <div className="produto-card">
+            <h4>{titulo}</h4>
+            <p>
+                <span>Categoria:</span> {categoria}
+            </p>
+            <p>
+                <span>Valor:</span> R${valor}
+            </p>
+            <p>
+                <span>Descrição:</span> <div className="descr">{descricao}</div>
+                <div className="leia"><button id="btn">Leia mais</button></div>
+            </p>
+            <div className="action">
+                <Link to="/">
+                    <BsPencil/>
+                </Link>
+                <button>
+                    <BsFillTrashFill/>
+                </button>
+            </div>
             
          </div>
     )
--- tela-principal/src/rotas/Publicacao.js
+++ tela-principal/src/rotas/Publicacao.js
@@ -1,41 +1,38 @@
-import { useState, useEffect } from 'react';
-import './App.css'
-import PublicacaoPage from '../componentes/publicacao';
-import axios from 'axios';
-
-
+import { useState, useEffect } from "react";
+import "./App.css";
+import PublicacaoPage from "../componentes/publicacao";
+import axios from "axios";
 
 function Publicacao() {
-  const [produtos, setProdutos] = useState([]) 
+  const [produtos, setProdutos] = useState([]);
 
-  const getProdutos = async()=>{
+  const getProdutos = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001");
 
-try {
-    const response = await axios.get('http://localhost:3001');
+      const data = response.data;
+      setProdutos(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    const data =response.data;
-    setProdutos(data)
-} 
-catch (error) {
-  console.log(error);
-}
-    
-  }
-
-   useEffect(() => {
-   
+  useEffect(() => {
     getProdutos();
-  },[])
+  }, []);
+  
   return (
-    <div className='publicacaoContainer'>
-      {produtos.length > 0 && 
-      produtos.map((produto)=> <PublicacaoPage
-       titulo={produto.titulo}
-       categoria={produto.categoria}
-       valor={produto.valor}
-      
-      />)}     
-</div>
-  )
-      }
+    <div className="publicacaoContainer">
+      {produtos.length > 0 &&
+        produtos.map((produto) => (
+          <PublicacaoPage
+            titulo={produto.titulo}
+            categoria={produto.categoria}
+            valor={produto.valor}
+            descricao={produto.descricao}
+          />
+        ))}
+    </div>
+  );
+}
 export default Publicacao;
